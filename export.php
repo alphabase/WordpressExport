2d332cc7e458029ca1589e76210be34dcac24ee4
Gerard Nijboer <alphabase@users.noreply.github.com>
2015-10-17 17:10:47 +0200

    Update Bootstrap CSS, remove Bootstrap JS

--- export.php
+++ export.php
@@ -24,14 +24,11 @@ setlocale(LC_ALL, 'nl_NL');
 
 <meta name="robots" content="noindex, nofollow" />
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
 <!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-
-<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 <style>
 @media all {
